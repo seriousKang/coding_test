5b043d4ce6b48bdc6d9b71ebd45b7af137a4e762
seriousKang <seriousKang@gmail.com>
2020-07-17 21:22:36 +0900

    최댓값과 최솟값

--- programmers/src/programmers/lv2/basic/MaxNumMinNum.java
+++ programmers/src/programmers/lv2/basic/MaxNumMinNum.java
@@ -1,5 +1,21 @@
 package programmers.lv2.basic;
 
-public class MaxNumMinNum {
+import java.util.Arrays;
 
+public class MaxNumMinNum {
+	public String solution(String s) {
+        String[] arr = s.split(" ");
+        int[] intArr = new int[arr.length];
+        
+        for(int i=0; i<arr.length; i++) {
+            intArr[i] = Integer.valueOf(arr[i]);
+        }
+        Arrays.sort(intArr);
+        //System.out.println(Arrays.toString(intArr));
+        
+        int min = intArr[0];
+        int max = intArr[intArr.length-1];
+        
+        return String.format("%d %d", min, max);
+    }
 }
