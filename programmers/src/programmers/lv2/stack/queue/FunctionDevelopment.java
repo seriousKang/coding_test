1e1566f06ca654e02488c6aec6d194f429b6532f
seriousKang <seriousKang@gmail.com>
2020-07-17 22:17:17 +0900

    기능개발

--- programmers/src/programmers/lv2/stack/queue/FunctionDevelopment.java
+++ programmers/src/programmers/lv2/stack/queue/FunctionDevelopment.java
@@ -1,5 +1,39 @@
 package programmers.lv2.stack.queue;
 
-public class FunctionDevelopment {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
+public class FunctionDevelopment {
+	public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        List<Integer> answerList = new ArrayList<Integer>();
+        
+        for(int i=0; i<progresses.length; i++) {
+            int remain = 100 - progresses[i];
+            int devPeriod;
+            
+            if(remain%speeds[i] == 0) {
+                devPeriod = remain / speeds[i];
+            } else {
+                devPeriod = remain / speeds[i] + 1;
+            }
+            
+            if(!queue.isEmpty() && devPeriod > queue.peek()) { 
+                answerList.add(queue.size());
+                queue.clear();
+            }
+            
+            queue.offer(devPeriod);
+        }
+        answerList.add(queue.size());
+        
+        int[] answerArr = new int[answerList.size()];
+        for(int i=0; i<answerList.size(); i++) {
+            answerArr[i] = answerList.get(i);
+        }
+        
+        return answerArr;
+    }
 }
