097a6c1f4336ad4ee4c49f835f92552cbccf8fa5
seriousKang <seriousKang@gmail.com>
2020-09-15 20:44:08 +0900

    Shuffle the Array

---
+++ leetcode/src/leetcode/easy/ShuffleTheArray.java
@@ -0,0 +1,14 @@
+package leetcode.easy;
+
+public class ShuffleTheArray {
+	public int[] shuffle(int[] nums, int n) {
+        int[] answer = new int[n*2];
+        
+        for(int i=0; i<n; i++) {
+            answer[i*2] = nums[i];
+            answer[i*2+1] = nums[n+i];
+        }
+        
+        return answer;
+    }
+}
