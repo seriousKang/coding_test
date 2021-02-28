6eee5de0e0c74f86f3e4c6b7383662b34c486c98
seriousKang <seriousKang@gmail.com>
2021-02-28 23:05:18 +0900

    보물

---
+++ baekjoon/src/baekjoon/Treasure.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Treasure {
+    static int N;
+    static int[] A;
+    static int[] B;
+    static int result = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        A = new int[N];
+        B = new int[N];
+
+        for(int i=0; i<N; i++) {
+            A[i] = sc.nextInt();
+        }
+        for(int i=0; i<N; i++) {
+            B[i] = sc.nextInt();
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i=0; i<N; i++) {
+            result += A[i] * B[N-i-1];
+        }
+        System.out.println(result);
+
+        sc.close();
+    }
+}
